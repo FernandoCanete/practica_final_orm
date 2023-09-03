8ae1cbda6407e0ce4fcbf0bfb065187727465b5a
FernandoCanete <fernandocanetegaray@gmail.com>
2023-09-03 16:44:36 +0000

    Implementacion de admin.py

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -3,6 +3,32 @@ from django.contrib import admin
 # Register your models here.
 from .models import Laboratorio, DirectorGeneral, Producto
 
-admin.site.register(Laboratorio)
-admin.site.register(DirectorGeneral)
-admin.site.register(Producto)
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+
+admin.site.register(Laboratorio, LaboratorioAdmin)
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio')
+    ordering = ('nombre',)
+
+admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio','f_fabricacion', 'p_costo', 'p_venta')
+   
+   
+    ordering = ('nombre','laboratorio') 
+   
+    list_display_links = ('nombre', 'laboratorio')
+
+    ñist_filter = ('nombre', 'laboratorio')
+
+    def f_fabricacion_year(self, obj):
+        return obj.f_fabricacion.year
+
+
+    f_fabricacion_year.short_description = "F Fabricacion"
+
+admin.site.register(Producto, ProductoAdmin)
